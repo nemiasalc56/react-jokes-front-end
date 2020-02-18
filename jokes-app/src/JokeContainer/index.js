9ad2ff4430c21a90dd040264e9abbe87912832af
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-18 14:48:32 -0600

    added joke list component

--- jokes-app/src/JokeContainer/index.js
+++ jokes-app/src/JokeContainer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import JokeList from '../JokeList'
 
 
 class JokeContainer extends Component {
@@ -11,6 +12,7 @@ class JokeContainer extends Component {
 	}
 
 	componentDidMount() {
+
 		this.getJokes()
 	}
 
@@ -18,9 +20,26 @@ class JokeContainer extends Component {
 	getJokes = async () => {
 		console.log("getJokes is getting called");
 		// get the url
+		const url = process.env.REACT_APP_API_URL + '/api/v1/jokes/'
 		// fetch the url
+		const jokesResponse = await fetch(url, {
+			// include credentials
+			credentials: 'include',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		// we need to add CORS in the backend to have access to this information
+		console.log(jokesResponse);
+
 		// get json
-		
+		const jokesJson = await jokesResponse.json()
+		console.log(jokesJson);
+		// get the jokes in state
+		this.setState({
+			jokes: jokesJson.data
+		})
 
 	}
 
@@ -28,6 +47,7 @@ class JokeContainer extends Component {
 		return (
 			<div>
 				<h2>JokeContainer</h2>
+				<JokeList />
 			</div>
 			)
 	}
---
+++ jokes-app/src/JokeList/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function JokeList(props) {
+
+	return(
+		<div>
+			<h2>JokeList</h2>
+		</div>
+		)
+}
+
+
+export default JokeList
