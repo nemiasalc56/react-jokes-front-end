b5621aa4921e11280217c98badf4dad1f1dc8aec
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-18 10:08:34 -0600

    added joke container componenet

---
+++ jokes-app/src/JokeContainer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class JokeContainer extends Component {
+
+	render() {
+		return (
+			<div>
+				<h2>JokeContainer</h2>
+			</div>
+			)
+	}
+}
+
+
+export default JokeContainer
