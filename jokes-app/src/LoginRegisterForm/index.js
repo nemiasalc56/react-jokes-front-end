7c12e18f7912d78f76a01e59252560f7daafab63
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-18 10:50:35 -0600

    login register form actually shows a form

--- jokes-app/src/LoginRegisterForm/index.js
+++ jokes-app/src/LoginRegisterForm/index.js
@@ -2,11 +2,68 @@ import React, { Component } from 'react'
 
 
 class LoginRegisterForm extends Component {
+	constructor(props) {
+		super(props)
 
+		this.state = {
+			firstName: '',
+			lastName: '',
+			email: '',
+			username: '',
+			password: ''
+		}
+	}
 	render() {
 		return (
 			<div>
 				<h2>LoginRegisterForm</h2>
+
+				<form>
+					<div>
+						<label>First name:</label>
+						<input
+							type="text"
+							name="firstName"
+							placeholder="Enter First name"
+						/>
+					</div>
+
+					<div>
+						<label>Last name:</label>
+						<input
+							type="text"
+							name="lastName"
+							placeholder="Enter lastName"
+						/>
+					</div>
+
+					<div>
+						<label>Email:</label>
+						<input
+							type="text"
+							name="email"
+							placeholder="Enter email"
+						/>
+					</div>
+
+					<div>
+						<label>Username:</label>
+						<input
+							type="text"
+							name="username"
+							placeholder="Enter username"
+						/>
+					</div>
+					
+					<div>
+						<label>Password:</label>
+						<input
+							type="password"
+							name="password"
+							placeholder="Enter password"
+						/>
+					</div>
+				</form>
 			</div>
 			)
 	}
